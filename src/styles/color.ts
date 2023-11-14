41ac9443283f25e6c5794ab7cb26592da0a8cad1
huy250901 <huyyy250901@gmail.com>
2023-11-14 08:04:46 +0700

    hi

---
+++ src/styles/color.ts
@@ -0,0 +1,26 @@
+const variables = {
+  colors: {
+    primaryColor: {
+      primaryLight: "#85CEFF",
+      primaryDark: "#069BFF",
+    },
+    accentColor: {
+      accentLight: "#D6AF88",
+      accentDark: "#069BFF",
+    },
+    accent: "#e67e22",
+    semantic: {
+      success: "#2ecc71",
+      warning: "#f39c12",
+      danger: "#e74c3c",
+      info: "#3498db",
+    },
+  },
+  fontSizes: {
+    small: "12px",
+    medium: "16px",
+    large: "20px",
+  },
+};
+
+export default variables;
